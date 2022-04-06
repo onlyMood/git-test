1678c37dbf9737ae2639531479e1f7c450669b82
sualihe <98069050+sualihe@users.noreply.github.com>
2022-04-06 18:56:58 +0800

    pull commit

--- src/main/java/com/mz/git/Git.java
+++ src/main/java/com/mz/git/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("hello,master");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
